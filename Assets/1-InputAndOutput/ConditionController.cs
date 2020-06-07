ae40ce35ba4e6af26f8c7db5d53cdfc677739194
Daisuke Suzuki <bboydaisuke@gmail.com>
2020-06-08 01:12:50 +0900

    ２週目の内容を追加した

---
+++ Assets/1-InputAndOutput/ConditionController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConditionController : MonoBehaviour
+{
+    void Start()
+    {
+        int a = 10;
+
+        Debug.Log("a の中身は " + a.ToString());
+
+        if (a == 5)
+        {
+            Debug.Log("a は 5 である");
+        }
+        else if (a > 5)
+        {
+            Debug.Log("a は 5 より大きい");
+        }
+        else
+        {
+            Debug.Log("a は 5 ではなく、5 より大きくもない");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
