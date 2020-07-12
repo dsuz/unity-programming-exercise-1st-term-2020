1acfa69f520067e41077d5e90561b3637f1a61ee
Daisuke Suzuki <bboydaisuke@gmail.com>
2020-07-13 03:01:18 +0900

    ５週目の内容とパッケージを追加した

--- Assets/1-InputAndOutput/LoopController.cs
+++ Assets/1-InputAndOutput/LoopController.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class LoopController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    string[] strArray = { "Apple", "Orange", "Banana", "Grape", "Kiwi" };
+
     void Start()
     {
-        
+        Loop1();
+        Loop2();
+        Loop3();
+        Loop4();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Loop1()
     {
-        
+        Debug.Log("Loop1 ==========");
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log(i);
+        }
+    }
+
+    void Loop2()
+    {
+        Debug.Log("Loop2 ==========");
+        int[] intArray = { 5, 3, 7, 8, 9 };
+        for (int i = 0; i < intArray.Length; i++)
+        {
+            int x = intArray[i] * 2;
+            Debug.Log(x);
+        }
+    }
+
+    void Loop3()
+    {
+        Debug.Log("Loop3 ==========");
+        for (int i = 0; i < strArray.Length; i++)
+        {
+            Debug.Log(strArray[i]);
+        }
+    }
+
+    void Loop4()
+    {
+        Debug.Log("Loop4 ==========");
+        foreach (string s in strArray)
+        {
+            Debug.Log(s);
+        }
     }
 }
